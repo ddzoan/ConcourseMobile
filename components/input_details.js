f018399c767037dc539b94ef6282890acbe4f1b2
Dan Dzoan <dandzoan@gmail.com>
2016-05-06 17:43:00 -0700

    show input details

--- components/input_details.js
+++ components/input_details.js
@@ -7,27 +7,75 @@ import {
 } from 'react-native';
 
 const BuildHeader = require('./build_header');
+const Icon = require('react-native-vector-icons/FontAwesome');
 
 class InputDetails extends Component {
   render() {
-    const {input} = this.props;
+    // const {input} = this.props;
     const builds = require('../sample_builds_response.json');
     const resources = require('../sample_resources_data.json');
+    const input = resources.inputs[0];
     const latestBuild = builds[0];
+
+    const metadata = input.metadata.map((line, i) => {
+      return (
+        <View key={i} style={styles.metadata}>
+          <Text style={[styles.name, styles.details]}>{line.name}</Text>
+          <Text style={[styles.value, styles.details]}>{line.value}</Text>
+        </View>
+      )
+    })
 
     return (
       <View>
-      <BuildHeader job_name={latestBuild.job_name} build_number={latestBuild.name} status={latestBuild.status} />
-        <Text style={styles.input}>{input.name}</Text>
+        <BuildHeader job_name={latestBuild.job_name} build_number={latestBuild.name} status={latestBuild.status} />
+        <View style={styles.jobBar}>
+          <Text style={styles.input}>{input.name}</Text>
+          <Icon style={styles.jobBarIcon} name="check" size={16} color="white" />
+        </View>
+        <View style={{padding: 10}}>
+          <Text style={styles.details}>ref {input.version.ref}</Text>
+          <Text style={styles.details}>{input.first_occurrence ? '' : 'using version of resource found in cache'}</Text>
+          {metadata}
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  input: {
+  metadata: {
+    flexDirection: 'row'
+  },
+  name: {
+    flex: 2
+  },
+  value: {
+    flex: 4
+  },
+  jobBar: {
     backgroundColor: '#5D6D7E',
-    color: 'white'
+    flexDirection: 'row',
+    height: 44,
+    marginBottom: 4,
+    alignItems: 'center',
+  },
+  input: {
+    color: 'white',
+    flex: 1,
+    paddingLeft: 10
+  },
+  jobBarIcon: {
+    paddingRight: 10,
+    flex: 0,
+    width: 44,
+    height: 44,
+    color: 'white',
+    padding: 14,
+    backgroundColor: '#2FCC71'
+  },
+  details: {
+    color: 'white',
   }
 });
 
